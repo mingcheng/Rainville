00cde1c243a12eda99e29545bdee28fdeb2ab263
mingcheng <lucky@gracecode.com>
2013-10-22 18:51:31 +0800

    Add donate feature.

--- src/com/gracecode/android/rain/ui/MainActivity.java
+++ src/com/gracecode/android/rain/ui/MainActivity.java
@@ -18,6 +18,7 @@ import com.gracecode.android.rain.adapter.ControlCenterAdapter;
 import com.gracecode.android.rain.helper.TypefaceHelper;
 import com.gracecode.android.rain.player.PlayManager;
 import com.gracecode.android.rain.serivce.PlayService;
+import com.gracecode.android.rain.ui.fragment.DonateDialogFragment;
 import com.gracecode.android.rain.ui.fragment.FrontPanelFragment;
 import com.gracecode.android.rain.ui.widget.SimplePanel;
 
@@ -28,6 +29,8 @@ public class MainActivity extends Activity {
     private Intent mServerIntent;
     private ViewPager mControlCenterContainer;
     private ControlCenterAdapter mControlCenterAdapter;
+    private DonateDialogFragment mDonationFragment;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,7 @@ public class MainActivity extends Activity {
         mFrontPanelFragment = new FrontPanelFragment();
         mControlCenterAdapter = new ControlCenterAdapter(getFragmentManager());
         mServerIntent = new Intent(this, PlayService.class);
+        mDonationFragment = new DonateDialogFragment(MainActivity.this);
 
         getFragmentManager()
                 .beginTransaction()
@@ -73,6 +77,10 @@ public class MainActivity extends Activity {
         mFrontPanelFragment.setFrontPanel(mFrontPanel);
         mFrontPanel.addSimplePanelListener(mFrontPanelFragment);
 
+        if (mDonationFragment.isNeedShow()) {
+            mDonationFragment.show(getFragmentManager(), "DonateDialog");
+        }
+
         startService(mServerIntent);
         bindService(mServerIntent, mConnection, Context.BIND_NOT_FOREGROUND);
 
@@ -84,9 +92,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-        if (!mPlayManager.isPlaying()) {
+        if (mPlayManager != null && !mPlayManager.isPlaying()) {
             stopService(mServerIntent);
         }
+
         unbindService(mConnection);
         FlurryAgent.onEndSession(this);
     }
